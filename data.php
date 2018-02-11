56777190e95253c980da9fdeb329896576c7d9cf
tarchi.giacomo2310 <tarchi.giacomo2310@gmail.com>
2018-02-11 12:00:25 -0500

    version 0.15.1

---
+++ data.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	
+		  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="css/style_data.css"/>
+  </head>
+  <body>
+    <?php
+		$surname = $_GET['cognome'];
+		$name = $_GET['name'];
+		$email = $_GET['e-mail'];
+		$password = $_GET['password'];
+		$Country = $_GET['COMBO'];
+		$Gender = $_GET['gender'];
+		$Patente = $_GET['Patente'];
+    ?>
+    <div class = "Conteiner" name = "Sign-up">
+        <div class="panel panel-default">
+         <div class = "panel-heading">
+            <center>
+              <h2><center>Riepilogo Dati</center></h2>
+            </center>  
+         </div>
+         <div class="panel-body">
+          <center>
+						  <p>Cognome : <?php echo $surname;?></p><br>
+							<p>Nome : <?php echo $name;?></p><br>
+							<p>Sesso : <?php echo $Gender?></p><br>
+							<p>Nazionalita : <?php echo $Country;?></p><br>
+							<p>Patente :<?php echo $Patente;?> </p><br>
+							<p>Email : <?php echo $email;?></p><br>
+							<p>Password : <?php echo $password;?></p><br>
+					</center>
+         </div>
+         <div class = "panel-footer">
+           <form action = "" method = "get">
+					 		<center>
+              		<p>
+                		<button id = "cancel" type = "submit" value = "Annulla">Annulla</button>
+                		<button id = "sign-up" type = "submit" value = "Registra">Registra</button>
+              		</p>
+						 	</center>
+					 </form>
+         </div>    
+        </div>
+    </div>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 		  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" type="text/css" href="style_index.css"/>
+      <link rel="stylesheet" type="text/css" href="css/style_index.css"/>
   </head>
   <body>
     <div class = "Conteiner" name = "Sign-up">
@@ -20,27 +20,48 @@
          </div>
          <div class="panel-body">
           <center>
-            <p>Cognome : <input type = "text" name = "cognome"/></p><br>
-            <p>Nome : <input type = "text" name = "nome"/></p><br>
-            <p>Sesso : Maschio<input type = "Radio" name = "gender" value = "M"/> Femmina <input type = "Radio" name = "gender" value = "F"/></p><br>
-            <p>Nazionalita : <select id = "combobox" name = "COMBO">
-                              <option name = "IT" value = "Italia">Italia</option>
-                              <option name = "EN" value = "UK">United Kingdom</option> 
-                              <option name = "USA" value = "USA">United States of America</option>
-                              <option name = "FRA" value = "France">France</option>
-                             </select></p><br>
-            <p>Patente : <input type = "checkbox" name = "Patente"/>Categ. A <input type = "checkbox" name = "Patente"/>Categ. B </p><br>
-            <p>Email : <input type = "text" name = "e-mail"/></p><br>
-            <p>Password : <input type = "text" name = "password"/></p><br>
-          </center>
+						<form action = "data.php" method = "get">
+							<p>Cognome : <input id = "SURNAME" type = "text" name = "cognome"></p><br>
+							<input type = "hidden" name = "text-surname" value = "<?php echo $surname;?>">
+							<p>Nome : <input id = "NAME" type = "text" name = "nome"></p><br>
+							<input type = "hidden" name = "text-name" value = "<?php echo $name;?>">
+							<p>Sesso : Maschio<input type = "Radio" name = "gender" value = "M"> Femmina <input type = "Radio" name = "gender" value = "F"></p><br>
+							<p>Nazionalita : <select id = "combobox" name = "COMBO">
+																<option name = "IT" value = "Italia">Italia</option>
+																<option name = "EN" value = "UK">United Kingdom</option> 
+																<option name = "USA" value = "USA">United States of America</option>
+																<option name = "FRA" value = "France">France</option>
+															 </select></p><br>
+							<p>Patente : <input type = "checkbox" name = "Patente">Categ. A <input type = "checkbox" name = "Patente">Categ. B </p><br>
+							<p>Email : <input id = "E-MAIL" type = "text" name = "e-mail"></p><br>
+							<input type = "hidden" name = "text-email" value = "<?php echo $email;?>">
+							<p>Password : <input id = "PASSWORD" type = "text" name = "password"></p><br>
+							<input type = "hidden" name = "text-password" value = "<?php echo $password;?>">
+						</form>
+					</center>
          </div>
          <div class = "panel-footer">
-           <center>
-              <p>
-                <button id = "cancel" type = "submit" name = "cancel" value = "Annulla">Annulla</button>
-                <button id = "confirm" type = "submit" name = "confirm" value = "Conferma">Conferma</button>
-              </p>
-           </center>
+           <form action = "data.php" method = "get">
+					 		<center>
+              		<p>
+                		<button id = "cancel" type = "submit" value = "Annulla">Annulla</button>
+                		<button id = "confirm" type = "submit" value = "Conferma" onclick = 
+														"<?php
+														$surname = $_GET['cognome'];
+														$name = $_GET['name'];
+														$email = $_GET['e-mail'];
+														$password = $_GET['password'];
+														$Country = $_GET['COMBO'];
+														$Gender = $_GET['gender'];
+														$Patente = $_GET['Patente'];
+														if((!isset($surname)) && (!isset($name)) && (!isset($email)) && (!isset($password)) && (!isset($Country)) && (!isset($Gender)) (!isset($Patente)))
+														{
+															
+														}
+														?>" >Conferma</button>
+              		</p>
+						 	</center>
+					 </form>
          </div>    
         </div>
     </div>
